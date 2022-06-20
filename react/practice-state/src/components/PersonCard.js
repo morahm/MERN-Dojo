db4326f8be23409048e1aba8059be37528c84e30
morahm <72052463+morahm@users.noreply.github.com>
2022-06-19 23:48:15 -0400

    initial commit

---
+++ react/practice-state/src/components/PersonCard.js
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+
+const PersonCard = (props) => {
+  // destructuring the props makes it easier to use in the component
+  //    but is NOT required
+  // We will not destructure age from props and will use it directly
+  //    in the JSX that is returned to demonstrate a different way to
+  //    use props
+
+const { firstName, lastName, hair } = props;
+
+const [state, setState] = useState({
+  greeting: "Knock knock, ",
+});
+setState({ greeting: state.greeting + "Neo" });
+console.log(state.greeting);
+
+
+
+
+
+
+
+
+  return (
+    <div className="container">
+      <h2>
+        {lastName}, {firstName}
+      </h2>
+      <p>Age: {props.age}</p>
+      <p>Hair Color: {hair}</p>
+  
+    </div>
+    
+  );
+};
+
+export default PersonCard;
